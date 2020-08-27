d7000f603d705d76b18f0912344179ee9a0d6949
oliveirasirley <65969793+syoliveira@users.noreply.github.com>
2020-08-27 20:51:06 -0300

    Delete exporter.py

--- exporter.py
+++
@@ -1,9 +0,0 @@
-import csv
-def toCSV(data_list):
-    with open('Familias.csv', 'w',encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Estado Civil', 'Filhos', 'Renda Total', 'Poupança', 'Despesa Filhos', 'Despesa Moradia', 'Valor Moradia 1','Aluguel Moradia 1','Valor Moradia 2','Aluguel Moradia 2','Valor Moradia 3','Aluguel Moradia 3'])
-        lista = []
-        for item in data_list:
-            lista.append(item)
-        writer.writerows(lista)
