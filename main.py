9593f613cf90f0e7f7312157470e2ddd87921e27
oliveirasirley <65969793+syoliveira@users.noreply.github.com>
2020-08-27 20:51:20 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from simulacao import Simulacao
-
-if __name__ == '__main__':
-    print("Família BeTa")
-    repeticoes = int(input("Quantas Famílias você deseja gerar ? "))
-    Simulacao.run(repeticoes) #chamada de função de simulação
