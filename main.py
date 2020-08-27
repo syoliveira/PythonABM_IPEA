4a41fd19bb53550c301c0afc4b00f3fd7eef95eb
oliveirasirley <65969793+syoliveira@users.noreply.github.com>
2020-08-27 20:41:12 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-from Familia import Familia
-from Imovel import Imovel
-import Exporter
-import Graficos
+from simulacao import Simulacao
 
-print("Famila BeTa")
-familias = int(input("Quantas familias você deseja gerar ? "))
-lista_familias = []
-for x in range(familias):
-    familia = Familia()
-    imovelA = Imovel("A")
-    imovelB = Imovel("B")
-    imovelC = Imovel("C")
-    print("Nova Familia: \n")
-    print(familia.__str__())
-    print(imovelA.__str__())
-    print(imovelB.__str__())
-    print(imovelC.__str__())
-    dados_familia = []
-    if familia.casado:
-        dados_familia.append("Casado")
-    else:
-        dados_familia.append("Solteiro")
-    dados_familia.append(str(familia.filhos))
-    dados_familia.append(str(round(familia.renda+familia.rendaConjuge,2)))
-    dados_familia.append(str(familia.poupanca))
-    dados_familia.append(str(familia.despesaFilhos()))
-    dados_familia.append(str(familia.despesaMoradia()))
-    dados_familia.append(str(imovelA.valor))
-    dados_familia.append(str(imovelA.aluguel))
-    dados_familia.append(str(imovelB.valor))
-    dados_familia.append(str(imovelB.aluguel))
-    dados_familia.append(str(imovelC.valor))
-    dados_familia.append(str(imovelC.aluguel))
-    lista_familias.append(dados_familia)
-Exporter.toCSV(lista_familias)
-lista_grafico=[]
-lista_num_graf = []
-for i in range(len(lista_familias)):
-    lista_grafico.append(float(lista_familias[i][2]))
-    lista_num_graf.append("Família "+str(int(+i+1)))
-Graficos.generate(lista_num_graf,lista_grafico)
+if __name__ == '__main__':
+    print("Família BeTa")
+    repeticoes = int(input("Quantas Famílias você deseja gerar ? "))
+    Simulacao.run(repeticoes) #chamada de função de simulação
---
+++ simulacao.py
@@ -0,0 +1,42 @@
+from familia import Familia
+from imovel import Imovel
+import exporter
+import graficos
+
+class Simulacao: #Criação da classe de simulação com a função run() para testes de repetições
+
+    def run(repeticoes):
+        lista_familias = []
+        for x in range(repeticoes):
+            familia = Familia()
+            imoveis = [Imovel("A"), Imovel("B"), Imovel("C")]
+            print("Nova Familia: \n")
+            print(familia.__str__())
+            print(imoveis[0].__str__())
+            print(imoveis[1].__str__())
+            print(imoveis[2].__str__())
+            print(familia.comprar(imoveis), "\n")  # adição de chamada da função compra
+            dados_familia = []
+            if familia.casado:
+                dados_familia.append("Casado")
+            else:
+                dados_familia.append("Solteiro")
+            dados_familia.append(str(familia.filhos))
+            dados_familia.append(str(round(familia.renda + familia.rendaConjuge, 2)))
+            dados_familia.append(str(familia.poupanca))
+            dados_familia.append(str(familia.despesaFilhos()))
+            dados_familia.append(str(familia.despesaMoradia()))
+            for i in range(3):  # adição de loop para substituir as repetições q eram possiveis de ser feitas com loop
+                dados_familia.append(str(imoveis[i].valor))
+                dados_familia.append(str(imoveis[i].aluguel))
+            lista_familias.append(dados_familia)
+        exporter.toCSV(lista_familias)
+        lista_grafico = []
+        lista_num_graf = []
+        for i in range(len(lista_familias)):
+            lista_grafico.append(float(lista_familias[i][2]))
+            lista_num_graf.append("Família " + str(int(+i + 1)))
+        graficos.generate(lista_num_graf, lista_grafico)
+
+if __name__ == '__main__':
+    Simulacao.run(10)
